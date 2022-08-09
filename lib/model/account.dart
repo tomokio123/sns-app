be19c5c4ef47e0f048210c58195284ad37925df3
t.fukuyama <t.fukuyama0123@gmail.com>
2022-08-09 21:34:39 +0900

    #3 クラスを定義

---
+++ lib/model/account.dart
@@ -0,0 +1,12 @@
+class Account {
+  String id;
+  String name;
+  String imagePath;
+  String selfIntroduction;
+  String userId;
+  DateTime? createdTime;
+  DateTime? updatedTime;
+
+  Account({this.id = '', this.name = '', this.imagePath = '',
+    this.selfIntroduction = '', this.userId = '', this.createdTime, this.updatedTime});
+}
--- lib/view/time_line/time_line_page.dart
+++ lib/view/time_line/time_line_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sns_app/model/account.dart';
 
 class TimeLinePage extends StatefulWidget {
   const TimeLinePage({Key? key}) : super(key: key);
@@ -8,6 +9,14 @@ class TimeLinePage extends StatefulWidget {
 }
 
 class _TimeLinePageState extends State<TimeLinePage> {
+  Account myAccount = Account(
+    id: '1',
+    name: 't.fukuyama',
+    selfIntroduction: 'Hello!',
+    userId: 't.fukuyama',
+    createdTime: DateTime.now(),
+    updatedTime: DateTime.now() //Accountのインスタンス作成
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
