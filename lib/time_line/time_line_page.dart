42a9d6fa1ea4f80df3e66836ba0fa929bf7e8801
t.fukuyama <t.fukuyama0123@gmail.com>
2022-08-09 20:39:04 +0900

    #2コードを整理

---
+++ lib/time_line/time_line_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TimeLinePage extends StatefulWidget {
+  const TimeLinePage({Key? key}) : super(key: key);
+
+  @override
+  _TimeLinePageState createState() => _TimeLinePageState();
+}
+
+class _TimeLinePageState extends State<TimeLinePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('タイムライン')),
+    );
+  }
+}
