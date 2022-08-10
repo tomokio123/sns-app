6bfec6d0325abea359d70e6284dc46faa79c7a83
t.fukuyama <t.fukuyama0123@gmail.com>
2022-08-10 14:36:19 +0900

    assetsディレクトリを追加

--- lib/view/account/account_page.dart
+++ lib/view/account/account_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../model/account.dart';
+import '../../model/post.dart';
+
 class AccountPage extends StatefulWidget {
   const AccountPage({Key? key}) : super(key: key);
 
@@ -9,8 +12,72 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPageState extends State<AccountPage> {
+
+  Account myAccount = Account(
+      id: '1',
+      name: '福山',
+      selfIntroduction: 'Hello!',
+      imagePath: '/Users/fukuyamatomoki/StudioProjects/sns_app/lib/assets/IMG_3621.jpg',
+      userId: 't.fukuyama',
+      createdTime: DateTime.now(),
+      updatedTime: DateTime.now() //Accountのインスタンス作成
+  );
+
+  List<Post> postList = [
+    Post(
+        id: '1',
+        content: 'Hello World1!',
+        postAccountId: '1',
+        createdTime: DateTime.now() //Postのインスタンスを作成
+    ),
+
+    Post(
+        id: '2',
+        content: 'Hello World2!',
+        postAccountId: '1',
+        createdTime: DateTime.now() //Postのインスタンスを作成
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.red,
+              height: 200,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: AssetImage(myAccount.imagePath)
+                            ),
+                          color: Colors.white
+                        ),
+                          width: 110.0,
+                          height: 110.0,
+                      ),
+                      Column(
+                        children: [
+                          Text(myAccount.name),
+                          Text(myAccount.userId)
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
